97165e7c6864b5d013e1566d88fad80f5d54653a
daryl herzmann <akrherz@iastate.edu>
2011-01-12 13:48:07 +0000

    use ISU styling and password protect the website for now

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,151 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+<title>sustainablecorn.org</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" media="all" href="css/isuP.css" />
+<style type="text/css">
+<!--
+.pullquote { font-family: verdana, arial, sans-serif; }
+h5 { font-size: 100%; font-weight: 600; color: #333; display: inline; }
+-->
+</style>
+<meta name="description" content="Homepage for sustainablecorn.org." />
+<meta name="keywords" content="sustainablecorn" />
 </head>
+
 <body>
-<h3>Nothing to see here, move along....</h3>
+<div id="container">
+
+<!-- BANNER -->
+
+<div id="masthead">
+
+<div id="nameplate">
+<a href="http://www.iastate.edu/">
+<img src="http://www.iastate.edu/img/04/isu200.gif" alt="Iowa State University" width="200" height="15" /></a>
+</div>
+
+<div id="index">
+<a id="indexlabel" href="http://www.iastate.edu/index/alpha/A.shtml">INDEX</a>
+<a class="abc" id="a" href="http://www.iastate.edu/index/alpha/A.shtml">A</a>
+<a class="abc" id="b" href="http://www.iastate.edu/index/alpha/B.shtml">B</a>
+<a class="abc" id="c" href="http://www.iastate.edu/index/alpha/C.shtml">C</a>
+<a class="abc" id="d" href="http://www.iastate.edu/index/alpha/D.shtml">D</a>
+
+<a class="abc" id="e" href="http://www.iastate.edu/index/alpha/E.shtml">E</a>
+<a class="abc" id="f" href="http://www.iastate.edu/index/alpha/F.shtml">F</a>
+<a class="abc" id="g" href="http://www.iastate.edu/index/alpha/G.shtml">G</a>
+<a class="abc" id="h" href="http://www.iastate.edu/index/alpha/H.shtml">H</a>
+<a class="abc" id="i" href="http://www.iastate.edu/index/alpha/I.shtml">I</a>
+<a class="abc" id="j" href="http://www.iastate.edu/index/alpha/J.shtml">J</a>
+<a class="abc" id="k" href="http://www.iastate.edu/index/alpha/K.shtml">K</a>
+<a class="abc" id="l" href="http://www.iastate.edu/index/alpha/L.shtml">L</a>
+<a class="abc" id="m" href="http://www.iastate.edu/index/alpha/M.shtml">M</a>
+
+<a class="abc" id="n" href="http://www.iastate.edu/index/alpha/N.shtml">N</a>
+<a class="abc" id="o" href="http://www.iastate.edu/index/alpha/O.shtml">O</a>
+<a class="abc" id="p" href="http://www.iastate.edu/index/alpha/P.shtml">P</a>
+<a class="abc" id="q" href="http://www.iastate.edu/index/alpha/Q.shtml">Q</a>
+<a class="abc" id="r" href="http://www.iastate.edu/index/alpha/R.shtml">R</a>
+<a class="abc" id="s" href="http://www.iastate.edu/index/alpha/S.shtml">S</a>
+<a class="abc" id="t" href="http://www.iastate.edu/index/alpha/T.shtml">T</a>
+<a class="abc" id="u" href="http://www.iastate.edu/index/alpha/U.shtml">U</a>
+<a class="abc" id="v" href="http://www.iastate.edu/index/alpha/V.shtml">V</a>
+
+<a class="abc" id="w" href="http://www.iastate.edu/index/alpha/W.shtml">W</a>
+<a class="abc" id="x" href="http://www.iastate.edu/index/alpha/X.shtml">X</a>
+<a class="abc" id="y" href="http://www.iastate.edu/index/alpha/Y.shtml">Y</a>
+<a class="abc" id="z" href="http://www.iastate.edu/index/alpha/Z.shtml">Z</a>
+</div>
+
+<div id="search">
+<fieldset>
+<form method="get" action="http://google.iastate.edu/search">
+<div>
+<a href="http://www.info.iastate.edu">E-Mail/Phones</a> |  
+
+<label for="isusearch">ISU Search</label>
+   <input type="hidden" name="output" value="xml_no_dtd"/>
+   <input type="hidden" name="client" value="default_frontend"/>
+   <input type="hidden" name="proxystylesheet" value="default_frontend"/>
+   <input type="hidden" name="" value=""/>
+   <input type="text" id="isusearch" name="q" size="18"></input>
+</div>
+</form>
+</fieldset>
+</div>
+</div>
+
+
+<!-- TITLE BAR -->
+
+<div id="titlebar">
+<h1>SustainableCorn.org</h1>
+</div>
+
+<table id="main" cellspacing="0">
+
+<tr>
+<td  id="a3" rowspan="2">
+<div id="mainlinks">
+
+<!-- INSERT LINKS IN LEFT COLUMN -->
+<div class="linkhead"><h3>Project Info:</h3></div>
+<ul>
+<li><a href="#">Primary Investigators</a></li>
+<li><a href="#">Project Objectives</a></li>
+<li><a href="#">Site Locations</a></li>
+
+</ul>
+
+<div class="linkhead"> <h3>Download Data</h3></div>
+<ul>
+<li><a href="#">Empty Vaults</a></li>
+</ul>
+
+
+
+
+</div>
+
+</td>
+
+<!-- 2ND COLUMN -->
+<td id="b3">
+<div class="maintext">
+<h2>Content Coming</h2>
+
+
+</div>
+</td>
+
+<td id="c3">
+<div class="maintext">
+
+<div class="pullquote">
+<h3>Upcoming Events</h3>
+<p>February meeting in Chicago</p>
+</div>
+
+</div>
+</td>
+</tr>
+</table>
+
+<!-- FOOTER AND COPYRIGHT INFO -->
+
+<div id="footer">
+<div id="copyright">
+<img src="http://www.iastate.edu/img/04/ISUbecoming.gif" alt="Becoming
+the Best" width="200" height="31" /> 
+
+<p>University Relations, (515) 294-4845, 
+<a href="mailto:online@iastate.edu">online@iastate.edu</a>.</p> 
+<p>Copyright &#169; 2011, Iowa State University of Science and Technology. All rights
+reserved.</p> </div> </div>
+
+</div>
 </body>
 </html>
