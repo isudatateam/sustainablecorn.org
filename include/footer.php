f6cd9e340c60336c68ef38d375f033d27eecd7bb
daryl herzmann <akrherz@iastate.edu>
2011-01-20 13:44:04 +0000

    add standard header and footer

---
+++ include/footer.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Created on Jan 12, 2011
+ *
+ * Standard footer
+ */
+?>
+<!-- FOOTER AND COPYRIGHT INFO -->
+
+<div id="footer">
+<div id="copyright">
+<img src="http://www.iastate.edu/img/04/ISUbecoming.gif" alt="Becoming
+the Best" width="200" height="31" /> 
+
+<p>College of Agriculture and Life Sciences, (515) 294-2518,
+<br />138 Curtiss Hall, Ames, IA 50011-1050
+<a href="mailto:cals@iastate.edu">cals@iastate.edu</a>.</p> 
+<p>Copyright &#169; 2011, Iowa State University of Science and Technology. All rights
+reserved.</p> </div> </div>
+
+</div>
+</body>
+</html>
---
+++ include/side.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Created on Jan 12, 2011
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+<div id="mainlinks">
+
+<!-- INSERT LINKS IN LEFT COLUMN -->
+<div class="linkhead"><h3>Project Info:</h3></div>
+<ul>
+<li><a href="#">Primary Investigators</a></li>
+<li><a href="#">Project Objectives</a></li>
+<li><a href="#">Site Locations</a></li>
+
+</ul>
+
+<div class="linkhead"> <h3>Download Data</h3></div>
+<ul>
+<li><a href="#">Empty Vaults</a></li>
+</ul>
+
+
+
+
+</div>
