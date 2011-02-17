a64b0ccfbc0b54dbe4c2687dc8c1dab950a8c963
daryl herzmann <akrherz@iastate.edu>
2011-02-17 18:41:57 +0000

    oops, need this page too!

---
+++ htdocs/press.php
@@ -0,0 +1,39 @@
+<?php 
+include("../config/settings.inc.php");
+$TITLE = "Press Releases";
+include(ROOTPATH."/include/header.php"); 
+?>
+
+<table id="main" cellspacing="0">
+
+<tr>
+<td  id="a2" rowspan="2" valign="top">
+<?php include(ROOTPATH."/include/side.php"); ?>
+
+</td>
+
+<!-- 2ND COLUMN -->
+<td id="b2" valign="top">
+<div class="maintext">
+
+<h3>Press Releases</h3>
+
+<table cellspacing="0" cellpadding="2" border="1">
+<head><tr><th>Date</th><th>Title</th><th>Formats</th></tr></thead>
+
+<tbody>
+<tr><td>18 Feb 2011</td><td>USDA Press Release</td>
+  <td><a href="pr/usda-release-110218.docx">MS Word</a></td></tr>
+<tr><td>18 Feb 2011</td><td>Iowa State Press Release</td>
+  <td><a href="pr/isu-release-110218.docx">MS Word</a></td></tr>
+</table>
+
+</div>
+</td>
+
+
+</div>
+</td>
+</tr>
+</table>
+<?php include(ROOTPATH."/include/footer.php"); ?>
