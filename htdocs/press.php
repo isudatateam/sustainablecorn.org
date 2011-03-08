5fbf509f7a6067f2bc981d227111812193941e35
daryl herzmann <akrherz@iastate.edu>
2011-03-08 20:56:39 +0000

    add Minnesota link

--- htdocs/press.php
+++ htdocs/press.php
@@ -32,6 +32,8 @@ include(ROOTPATH."/include/header.php");
   <td><a href="pr/sdstate-release-110218.docx">MS Word</a></td></tr>
 <tr><td>18 Feb 2011</td><td>Ohio State Press Release</td>
   <td><a href="http://extension.osu.edu/news-releases/archives/2011/february/ohio-state-shares-in-20m-usda-grant-for-sustainability-of-corn-production-in-the-midwest">Website</a></td></tr>
+<tr><td>18 Feb 2011</td><td>Minnesota</td>
+  <td><a href="http://swroc.cfans.umn.edu/prod/groups/cfans/@pub/@cfans/@swroc/documents/asset/cfans_asset_312051.pdf">PDF</a></td></tr>
 <tr><td>21 Feb 2011</td><td>Purdue Press Release</td>
   <td><a href="pr/purdue-release-110221.doc">MS Word</a></td></tr>
 <tr><td>4 March 2011</td><td>Missouri Press Release</td>
