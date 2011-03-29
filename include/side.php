3865caa4d35b8cb5b0213e0ab49a0beef1385118
daryl herzmann <akrherz@iastate.edu>
2011-03-29 12:58:26 +0000

    link to google collaboration website and remove unused side links

--- include/side.php
+++ include/side.php
@@ -13,15 +13,16 @@
 <ul>
 <li><a href="/">Home</a></li>
 <li><a href="contact.php">Contact</a></li>
-<li>Education/Outreach</li>
-<li>Extension</li>
+<!-- li>Education/Outreach</li>
+<li>Extension</li> -->
 <li><a href="press.php">Press Releases</a></li>
 <li><a href="team.php">Primary Investigators</a></li>
-<li>Progress Report</li>
+<!-- <li>Progress Report</li>
 <li>Project Structure</li>
 <li>Publications</li>
-<li>Research</li>
+<li>Research</li> -->
 <li><a href="rules.php">Rules of Engagement</a></li>
+<li><a href="https://sites.google.com/site/sustainablecorn/">Internal Information</a></li>
 </ul>
 
 
