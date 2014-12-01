f45ea8de33fb038eae2c7ba399936f08bcffbe55
daryl herzmann <akrherz@iastate.edu>
2014-12-01 20:36:09 +0000

    sigh

--- htdocs/PDF_download.php
+++ htdocs/PDF_download.php
@@ -10,6 +10,7 @@ if (isset($_GET["d"])){
   if (!endsWith($f, ".pdf")){
     die(file_get_contents($f));
   }
+  die();
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
